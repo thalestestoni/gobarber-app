2882d411eef893e050974ddba34c23bccfd114c2
Thales <thalestestoni@gmail.com>
2020-10-03 11:56:51 -0300

    Register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import SignIn from './pages/Signin';
+import Signup from './pages/Signup';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <Signup />
     <GlobalStyle />
   </>
 );
